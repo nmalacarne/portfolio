4bbf5865aa9e619df9148ab2bf8787573f3abc3d
Nicholas Malacarne <nick@craterdust.com>
2015-05-07 14:42:47 -0400

    moved stylus file location; jake task now handles stylus compilation

--- Jakefile.js
+++ Jakefile.js
@@ -1,5 +1,12 @@
+var web = require('jake-web-utils');
+
 desc('serves the application via nodemon');
-task('serve', {async: true}, function () {
+task('serve', function () {
+    web.watchStylus({
+        src: ['./stylus/*.styl'],
+        dest: './public/stylesheets/'
+    });
+
     jake.exec('nodemon bin/www', {interactive: true}, function () {
         complete();
     });
