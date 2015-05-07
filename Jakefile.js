0f2f85a62301536696c15ecd48236b6446773c25
Nicholas Malacarne <nick@craterdust.com>
2015-05-07 16:03:18 -0400

    added jake build task

--- Jakefile.js
+++ Jakefile.js
@@ -1,13 +1,20 @@
 var web = require('jake-web-utils');
 
+var stylusOpts = {
+    src: ['./stylus/*.styl'],
+    dest: './public/stylesheets/'
+};
+
 desc('serves the application via nodemon');
 task('serve', function () {
-    web.watchStylus({
-        src: ['./stylus/*.styl'],
-        dest: './public/stylesheets/'
-    });
+    web.watchStylus(stylusOpts);
 
     jake.exec('nodemon bin/www', {interactive: true}, function () {
         complete();
     });
+});
+
+desc('builds stylus files');
+task('build', function () {
+   web.compileStylus(stylusOpts);
 });
